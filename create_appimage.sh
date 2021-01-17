f0807144ff0a6180a87ea0dc46bb0a85d7ba5cd6
jsj1027 <jsj1027@g.rit.edu>
2021-01-17 15:43:59 -0500

    v2.2

--- create_appimage.sh
+++ create_appimage.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+set -e
+
+cd Ishiiruka
+
 ZSYNC_STRING="gh-releases-zsync|Birdthulu|FPM-AppImage|latest|Faster_Project_Plus-x86_64.AppImage.zsync"
 
 LINUXDEPLOY_PATH="https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous"
@@ -14,7 +18,7 @@ UPDATETOOL_PATH="https://github.com/AppImage/AppImageUpdate/releases/download/co
 UPDATETOOL_FILE="appimageupdatetool-x86_64.AppImage"
 UPDATETOOL_URL="${UPDATETOOL_PATH}/${UPDATETOOL_FILE}"
 
-APPDIR_BIN="./AppDir/usr/bin"
+APPDIR_BIN="./build/AppDir/usr/bin"
 
 # Grab various appimage binaries from GitHub if we don't have them
 if [ ! -e ./Tools/linuxdeploy ]; then
@@ -30,27 +34,14 @@ if [ ! -e ./Tools/appimageupdatetool ]; then
 	chmod +x ./Tools/appimageupdatetool
 fi
 
-# Delete the AppDir folder to prevent build issues
-rm -rf ./AppDir/
-
-# Build the AppDir directory for this image
-mkdir -p AppDir
-./Tools/linuxdeploy \
-	--appdir=./AppDir \
-	-e ./build/Binaries/ishiiruka \
-	-d ./Data/ishiiruka.desktop \
-	-i ./Data/ishiiruka.png
+./Tools/linuxdeploy --appdir=./build/AppDir --executable ./build/Binaries/ishiiruka -d ./Data/ishiiruka.desktop -i ./Data/ishiiruka.png
 
 # Add the Sys dir to the AppDir for packaging
 cp -r Data/Sys ${APPDIR_BIN}
 
-# echo "Using Netplay build config"
-
-# rm -f ${NETPLAY_APPIMAGE_STRING}
-
 # Package up the update tool within the AppImage
-cp ./Tools/appimageupdatetool ./AppDir/usr/bin/
+cp ./Tools/appimageupdatetool ${APPDIR_BIN}
 
 # Bake an AppImage with the update metadata
-UPDATE_INFORMATION="${ZSYNC_STRING}" \
-    ./Tools/linuxdeploy-update-plugin --appdir=./AppDir/
+export UPDATE_INFORMATION="${ZSYNC_STRING}"; 
+./Tools/linuxdeploy-update-plugin --appdir=./build/AppDir/;
